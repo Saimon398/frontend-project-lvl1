05a31c1266fd4bbf12c0534baad2d4bafd37c359
alex_p <aleksborislavskij@gmail.com>
2022-03-31 17:27:10 +0300

    installed readline-sync package

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
+
+// Исполняемый скрипт 
+
+import introduceUser from '../src/cli.js';
+
 console.log('Welcome to the Brain Games!');
+
+introduceUser(); // Выполнение происходит здесь
---
+++ src/cli.js
@@ -0,0 +1,10 @@
+import readlineSync from 'readline-sync';
+
+// Модуль
+
+const introduceUser = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+}
+
+export default introduceUser;
